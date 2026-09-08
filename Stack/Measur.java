b80e401b27b0973d6c1d3068f9dc8e8906c4a914
jaladurgamnagatharun <jaladurgamnagatharun@gmail.com>
2026-09-08 18:05:12 +0530

    Added Stack DSA code

---
+++ Stack/Measur.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Measur {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.nextLine();
+    while (n-- > 0) {
+      String s = sc.nextLine();
+      System.out.println(longestValid(s));
+    }
+  }
+
+  public static int longestValid(String s) {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(-1);
+    int maxLen = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        stack.push(i);
+      } else {
+        stack.pop();
+        if (stack.isEmpty()) {
+          stack.push(i);
+        } else {
+          maxLen = Math.max(maxLen, i - stack.peek());
+        }
+      }
+    }
+    return maxLen;
+  }
+}
